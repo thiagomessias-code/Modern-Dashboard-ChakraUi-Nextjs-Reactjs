f02209ada2070b33f298b3de0c117ccdfa4942df
Thiago Messias <thiagomessiasdevbr@gmail.com>
2022-03-22 21:41:42 +0000

    Usuarios dash

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, Icon } from '@chakra-ui/react'
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from '@chakra-ui/react'
 import Header from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
 import {RiUserAddLine} from 'react-icons/ri'
@@ -24,6 +24,33 @@ export default function UserList() {
 
                         <Button as="a" fontSize="sm" colorScheme="pink" size="sm" leftIcon={<Icon as={RiUserAddLine}/>}>Criar Novo</Button>
                     </Flex>
+
+                    <Table colorScheme="whiteAlpha">
+                        <Thead>
+                            <Tr>
+                                <Th px="6" color="gray.300" width="8">
+                                    <Checkbox colorScheme="pink"/>
+                                </Th>
+
+                                <Th>Usuario</Th>
+                                <Th>Data de Cadastro</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            <Tr>
+                                <Td px="6">
+                                <Checkbox colorScheme="pink"/>
+                                </Td>
+                                <Td>
+
+                                    <Box>
+                                        <Text fontWeight="bold" >Thiago Messias</Text>
+                                        <Text fontSize="sm" >Thiagomessiasdebr@gmail.com</Text>
+                                    </Box>
+                                </Td>
+                            </Tr>
+                        </Tbody>
+                    </Table>
                 </Box>
 
             </Flex>
